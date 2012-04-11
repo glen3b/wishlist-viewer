fac65a60564f2303f3cd2cf602669acac5721cdf
Glen Husman <wappokickingdeer@gmail.com>
2012-04-10 18:29:37 -0700

    Added a legal notice

--- Wishlist-Client-Improved/src/org/glen_h/teacherwishlist/client_plus/WishlistViewerActivity.java
+++ Wishlist-Client-Improved/src/org/glen_h/teacherwishlist/client_plus/WishlistViewerActivity.java
@@ -1,3 +1,20 @@
+/*  Wishlist Viewer - View online wishlists from your android phone or tablet
+    Copyright (C) 2012  Glen Husman
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.glen_h.teacherwishlist.client_plus;
 
 
